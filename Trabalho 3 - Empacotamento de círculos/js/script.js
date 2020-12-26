b0e2b22101569161145a71123f4100cee973648b
Gabrielly de Andrade <gabrielly.andrade@poli.ufrj.br>
2020-12-26 12:15:45 -0300

    T3: modify base of work and implementing start and pause buttons

--- Trabalho 3 - Empacotamento de círculos/js/script.js
+++ Trabalho 3 - Empacotamento de círculos/js/script.js
@@ -1,5 +1,8 @@
-circles = [];
-minimunRadios = 2;
+var circles = [];
+var minimunRadios = 2;
+var circlesQuantity = 0;
+var interval;
+var pause = false;
 
 
 function getRandomColor() {
@@ -23,13 +26,15 @@ function generateRandomPoint(xmax, ymax){
 function calculateDistance(point){
     dmax = 700;
 
-    if (circles.lenght > 0){
-        console.log("lista com conteúdo");
-    }
-    else {
-        console.log("entrei no else");
-        return Math.floor(Math.random() * dmax);
-    }
+    // if (circles.lenght > 0){
+    //     console.log("lista com conteúdo");
+    // }
+    // else {
+    //     console.log("entrei no else");
+    //     return Math.floor(Math.random() * dmax);
+    // }
+
+    return Math.floor(Math.random() * dmax);
 
 }
 
@@ -41,9 +46,10 @@ function generateRandomRadios(x, y, maximunRadios){
     return radios;
 }
 
-function drawCircle(top, left, width){
+function createCircle(left, top, width, id){
 
-    const circle = document.createElement("div");
+    var circle = document.createElement("div");
+    circle.id = "circle-" + id;
 
     circle.style.borderRadius = "50%";
     circle.style.backgroundColor = getRandomColor();
@@ -54,12 +60,12 @@ function drawCircle(top, left, width){
     circle.style.display = "block";
 
     document.getElementById("circle-container").appendChild(circle);
+
+    console.log(document.getElementById("teste-1"));
+
 }
 
-document.getElementById("start").onclick = function(){
-    console.log("inicio");
-
-    // Valores máximos são representados pelo tamanho da div
+function drawCircle(){
     point = generateRandomPoint(1300, 700);
     console.log("meu x:" + point[0] + " meu y:" + point[1]);
 
@@ -71,11 +77,45 @@ document.getElementById("start").onclick = function(){
 
         radios = generateRandomRadios(point[0], point[1], distance);
 
+        circlesQuantity += 1;
+
         // Desenha circulo no espaço
-        drawCircle(point[1], point[0], radios);
-
+        createCircle(point[1], point[0], radios, circlesQuantity);
+        // drawCircle(circles[0][0], circles[0][1], circles[0][2]);
+        
         circles.push([point[0], point[1], radios]);
-
-
+        document.getElementById("circle-quantity").innerHTML = circlesQuantity;
     }
+    else {
+        console.log("distancia pequena");
+    }
+
 }
+
+
+function poll(){
+    interval = setInterval(drawCircle, 2000);
+}
+
+
+
+document.getElementById("start").addEventListener("click", function(){
+    clearInterval(interval);
+    document.getElementById("circle-container").innerHTML = "";
+    document.getElementById("circle-quantity").innerHTML = 0;
+    circlesQuantity = 0;
+    poll();
+});
+
+document.getElementById("pause").onclick = function(){
+    pause = !pause;
+
+    if (pause){
+        clearInterval(interval);
+        
+    }
+    else {
+        poll();
+    }
+    
+} 
